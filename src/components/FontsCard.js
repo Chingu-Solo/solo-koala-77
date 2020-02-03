8b9732bb1ffd4578dc8d5d83866a697ead1a2e37
Vincenzo <ilcontevince@gmail.com>
2020-02-03 22:21:04 +0000

    removee useEffect for Observer

--- src/components/FontsCard.js
+++ src/components/FontsCard.js
@@ -20,41 +20,41 @@ const FontsCard = props => {
 
   // <---------------------------
   // INTERSECTION OBSERVER \\
-  useEffect(() => {
-    // This option array let you control when the observer is invoked
-    const options = {
-      root: null, // the elements that is used as viewport ( default to the browser viewport if not specified)
-      rootMargin: "0px", // margin around the root before computing intersections ( default to all zeros)
-      threshold: 1 // between 0 - 1 ( default is 0, means as soon as is visible, 1 needs to be fully visible)
-    };
-    // creating the observer
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-          // if it's not in the root viewport
-          return; // do nothing
-        }
-        // else
-        // convert the Font with the valid format in order to import it (i.e Open+Sans)
-        const fontLink = entry.target.firstChild.firstChild.data
-          .split(" ")
-          .join("+");
-        // append a <link> to the <head> for the viewed elements
-        const link = document.createElement("link");
-        link.href = `https://fonts.googleapis.com/css?family=${fontLink}&display=swap`;
-        link.rel = "stylesheet";
-        link.type = "text/css";
-        document.head.appendChild(link);
-        // unobserve if the element is not in the viewport
-        console.log(entry.target);
-        observer.unobserve(entry.target);
-      });
-    }, options);
-    // Observe each element in the nodeElements array
-    nodeElements.forEach(element => {
-      observer.observe(element);
+
+  // This option array let you control when the observer is invoked
+  const options = {
+    root: null, // the elements that is used as viewport ( default to the browser viewport if not specified)
+    rootMargin: "0px", // margin around the root before computing intersections ( default to all zeros)
+    threshold: 1 // between 0 - 1 ( default is 0, means as soon as is visible, 1 needs to be fully visible)
+  };
+  // creating the observer
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        // if it's not in the root viewport
+        return; // do nothing
+      }
+      // else
+      // convert the Font with the valid format in order to import it (i.e Open+Sans)
+      const fontLink = entry.target.firstChild.firstChild.data
+        .split(" ")
+        .join("+");
+      // append a <link> to the <head> for the viewed elements
+      const link = document.createElement("link");
+      link.href = `https://fonts.googleapis.com/css?family=${fontLink}&display=swap`;
+      link.rel = "stylesheet";
+      link.type = "text/css";
+      document.head.appendChild(link);
+      // unobserve if the element is not in the viewport
+      console.log(entry.target);
+      observer.unobserve(entry.target);
     });
-  }, [nodeElements]);
+  }, options);
+  // Observe each element in the nodeElements array
+  nodeElements.forEach(element => {
+    observer.observe(element);
+  });
+
   // --------------------------->
 
   const fonts = props.fontsObject.map((font, index) => {
