a5150edbc08561dc3a5be76b0b520c2937b9aebe
Vincenzo <ilcontevince@gmail.com>
2020-02-08 19:20:01 +0000

    add search Font filter on search type

--- src/components/FontsCard.js
+++ src/components/FontsCard.js
@@ -6,21 +6,43 @@ import Card from "./Card";
 
 // === COMPONENT === \\
 const FontsCard = props => {
+  const fontRegex = new RegExp(props.searchValue, "i");
+  const fontFilter = props.fontsObject.filter(font =>
+    font.family.match(fontRegex)
+  );
   return (
     <Wrap className="cardWrap">
-      {props.fontsObject.map((font, index) => {
-        return (
-          <LazyLoad height={120} once key={index}>
-            <Card
-              font={font}
-              index={index}
-              searchValue={props.searchValue}
-              typeValue={props.typeValue}
-              fontsObject={props.fontsObject}
-            />
-          </LazyLoad>
-        );
-      })}
+      {props.searchValue.length === 0 ? (
+        props.fontsObject.map((font, index) => {
+          return (
+            <LazyLoad height={120} once key={index}>
+              <Card
+                font={font}
+                index={index}
+                searchValue={props.searchValue}
+                typeValue={props.typeValue}
+                fontsObject={props.fontsObject}
+              />
+            </LazyLoad>
+          );
+        })
+      ) : fontFilter ? (
+        fontFilter.map((font, index) => {
+          return (
+            <LazyLoad height={120} once key={index}>
+              <Card
+                font={font}
+                index={index}
+                searchValue={props.searchValue}
+                typeValue={props.typeValue}
+                fontsObject={props.fontsObject}
+              />
+            </LazyLoad>
+          );
+        })
+      ) : (
+        <div>Not Found</div>
+      )}
     </Wrap>
   );
 };
