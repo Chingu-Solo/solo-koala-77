aa9dc988a7dc71c37f378cd14908a08a3944b969
Vincenzo <ilcontevince@gmail.com>
2020-02-09 15:40:22 +0000

    add ({...props}) + fontFilter.length

--- src/components/toolbar/Search.js
+++ src/components/toolbar/Search.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
-const Search = () => {
-  const onSearchType = event => {};
-  return <Wrap></Wrap>;
+const Search = ({ onSearchValue }) => {
+  // get the value of the Search Input
+  const searchChange = e => {
+    return onSearchValue(e.target.value); // get the value and pass the value to the callback (parameter) so it's accessible by the parent
+  };
+  return (
+    <MySearch>
+      <FontAwesomeIcon icon="search" />
+      <input type="text" placeholder="Search font" onChange={searchChange} />
+    </MySearch>
+  );
 };
 
 //* styled-component < 💅>
-const Wrap = styled.div``;
+const MySearch = styled.div``;
 
 export default Search;
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,52 +1,29 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import GoogleFonts from "../../apis/GoogleFonts";
 import styled from "styled-components";
-// import Search from "./Search";
+import Search from "./Search";
+import TextModifier from "./TextModifier";
+import FontSize from "./FontSize";
+import DarkMode from "./DarkMode";
+import ViewMode from "./ViewMode";
+import Reset from "./Reset";
 
-const Toolbar = ({ onSearchValue, onTypeValue }) => {
-  // get the value of the Search Input
-  const searchChange = e => {
-    return onSearchValue(e.target.value); // get the value and pass the value to the callback (parameter) so it's accessible by the parent
-  };
-  // get the value of the Type Input
-  const typeChange = e => {
-    return onTypeValue(e.target.value); // get the value pass the value to the callback (parameter) so it's accessible by the parent
-  };
-
+const Toolbar = ({ ...props }) => {
+  console.log(props);
   return (
-    <Wrap>
-      <Search placeholder="Search font" onChange={searchChange} />
-      <TextModifier placeholder="Type something" onChange={typeChange} />
-      <FontSize>
-        <FontAwesomeIcon icon="font" />
-      </FontSize>
-      <span>
-        <BackgroundSwitch>
-          <FontAwesomeIcon icon="sun" />
-        </BackgroundSwitch>
-        <BackgroundSwitch>
-          <FontAwesomeIcon icon="moon" />
-        </BackgroundSwitch>
-      </span>
-      <ViewMode>
-        <FontAwesomeIcon icon="list-alt" />
-      </ViewMode>
-      <Reset>
-        <FontAwesomeIcon icon="redo-alt" />
-      </Reset>
+    <Wrap className="toolbar">
+      <Search onSearchValue={props.onSearchValue} />
+      <TextModifier onTypeValue={props.onTypeValue} />
+      <FontSize />
+      <DarkMode />
+      <ViewMode />
+      <Reset />
     </Wrap>
   );
 };
 
 //* styled-component < 💅>
-const Wrap = styled.div``;
-
-const Search = styled.input``;
-const TextModifier = styled.input``;
-const FontSize = styled.button``;
-const BackgroundSwitch = styled.button``;
-const ViewMode = styled.button``;
-const Reset = styled.button``;
+const Wrap = styled.div`
+  display: flex;
+`;
 
 export default Toolbar;
