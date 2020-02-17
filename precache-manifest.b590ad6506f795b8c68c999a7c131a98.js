0c1c85c56761b58f110504c8d9d5ced814f887f0
Vincenzo <ilcontevince@gmail.com>
2020-02-17 12:01:10 +0000

    Updates

---
+++ precache-manifest.b590ad6506f795b8c68c999a7c131a98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c88e2a00e75ad14325883cd6af0a95",
+    "url": "/solo-koala-77/index.html"
+  },
+  {
+    "revision": "568e51f3f969f33fe399",
+    "url": "/solo-koala-77/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "568e51f3f969f33fe399",
+    "url": "/solo-koala-77/static/js/2.695c272c.chunk.js"
+  },
+  {
+    "revision": "86f96943d6537349861c4002a14026e3",
+    "url": "/solo-koala-77/static/js/2.695c272c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e75cc58942bd19e4d04c",
+    "url": "/solo-koala-77/static/js/main.1f08374c.chunk.js"
+  },
+  {
+    "revision": "51686a45eb84e474efde",
+    "url": "/solo-koala-77/static/js/runtime-main.a0d5b1e6.js"
+  }
+]);
